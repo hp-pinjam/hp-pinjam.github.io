d93b87325bd9d342f576350a7de149ff0f236ba8
ludha <1214002@std.ulbi.ac.id>
2023-12-02 21:28:03 +0700

    push

--- js/crooot.js
+++ js/crooot.js
@@ -2,10 +2,10 @@ import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
 
-export default function PostSignUp(){
-    let target_url = "https://us-central1-pyoyek3.cloudfunctions.net/Login";
+function PostLogIn(){
+    let target_url = "https://asia-southeast2-hppinjam.cloudfunctions.net/Login";
     let tokenkey = "token";
-    let tokenvalue = "40e27523381e236813ece16a5e420192592d2ed80cd74eafe115afbaebe874d2"; 
+    let tokenvalue = "bda91243ee1ba5363f95a7fd11e6c61df3b7f121669c57e6e1e4a8965674d787eab8142c84a2d9a7f01b812e606f042c9b7ad23a38b23ada8f72fc5cc70f0cc7"; 
     let datainjson = {
         "username": getValue("username"),
         "password": getValue("password")
@@ -15,17 +15,16 @@ export default function PostSignUp(){
 
 }
 function responseData(result) {
-    setInner("pesan", result.message);
-    setCookieWithExpireHour("token", result.token, 2);
     
-    if (result.message === "Selamat Datang") {
+    alert(result.message)
+    if (result.status === true) {
+        setCookieWithExpireHour("token", result.token, 2);
         // Jika pesan adalah "Selamat Datang", arahkan ke halaman dashboard.
-        window.location.href = "halaman.html"; // Gantilah "error.html" dengan halaman error yang sesuai.
-    } else if (result.message === "Password Salah") {
-        // Jika pesan kesalahan adalah "Password salah", arahkan ke halaman error.
-        window.location.href = "404.html";
+        window.location.href = "hp.html"; // Gantilah "error.html" dengan halaman error yang sesuai.
     } else {
         // Penanganan lainnya (pesan kesalahan lainnya)
-        window.location.href = "index.html";
+        window.location.href = "login.html";
     }
 }
+
+document.getElementById("buttonlogin").addEventListener("click",PostLogIn)
